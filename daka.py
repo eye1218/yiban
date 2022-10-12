97abc8969b23590c92903aba9da5b1b68d3455d4
李文康 <liwenkang@qq.com>
2022-10-12 21:10:30 +0800

    调整函数所在的模块位置

--- daka.py
+++ daka.py
@@ -9,7 +9,7 @@ import healthy_report
 import log
 import qq
 from models import Session as TSession
-from models import StudentInfo, Attendance, AttendanceHistory
+from models import StudentInfo, Attendance
 
 
 def daka():
@@ -28,8 +28,10 @@ def daka():
         for item in students:
             qq_number = session.query(StudentInfo).filter(StudentInfo.number == item.username).first().qq
             try:
-                result = healthy_report.healthy_report(username=item.username, session_id=session.query(TSession).filter(
-                    TSession.username == item.username).first().session_id, xm_id=xm_id)
+                result = healthy_report.healthy_report(username=item.username,
+                                                       session_id=session.query(TSession).filter(
+                                                           TSession.username == item.username).first().session_id,
+                                                       xm_id=xm_id)
                 if result:
                     message = f'{during_name}: 打卡成功 {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
                     session.query(Attendance).filter(Attendance.username == item.username).update(values={during: 1})
@@ -47,44 +49,6 @@ def daka():
                 log.file_logger.exception(e)
                 log.console_logger.exception(e)
         session.commit()
-    except Exception as e:
-        log.file_logger.exception(e)
-        log.console_logger.exception(e)
-
-
-def reset():
-    """
-    将Attendance表里的数据重置
-    :return:
-    """
-    try:
-        session = config.get_session()
-        session.query(Attendance).update({'morning': 0, 'afternoon': 0})
-        session.commit()
-        log.file_logger.info('Attendance表已重置')
-    except Exception as e:
-        log.file_logger.exception(e)
-        log.console_logger.exception(e)
-
-
-def store_data():
-    """
-    存储打卡记录
-    :return:
-    """
-    try:
-        session = config.get_session()
-        messages = session.query(Attendance)
-        for item in messages:
-            obj = AttendanceHistory(
-                username=item.username,
-                date=datetime.date.today(),
-                morning=item.morning,
-                afternoon=item.afternoon,
-            )
-            session.add(obj)
-        session.commit()
-        log.file_logger.info('Attendance表的数据已备份至AttendanceHistory表中')
     except Exception as e:
         log.file_logger.exception(e)
         log.console_logger.exception(e)
